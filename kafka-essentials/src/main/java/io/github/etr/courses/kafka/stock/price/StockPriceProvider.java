509ff9a3b777e1841900ed0215e6da93e8b10f5b
etrandafir93 <emanueltrandafir1993@gmail.com>
2025-06-28 18:10:26 +0300

    cleanup

--- kafka-essentials/src/main/java/io/github/etr/courses/kafka/stock/price/StockPriceProvider.java
+++ kafka-essentials/src/main/java/io/github/etr/courses/kafka/stock/price/StockPriceProvider.java
@@ -2,29 +2,33 @@ package io.github.etr.courses.kafka.stock.price;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/stocks")
 @RequiredArgsConstructor
-@Slf4j
 public class StockPriceProvider {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String TOPIC_NAME = "stock.price.update";
 
     @PutMapping("/{ticker}/price")
-    public void updateStockPrice(@PathVariable String ticker, @RequestParam String price) {
+    public ResponseEntity<Object> updateStockPrice(@PathVariable String ticker, @RequestParam String price) {
         log.info("Received request to update stock price for {}: {}", ticker, price);
         String messagePayload = ticker + ":" + price;
+
         try {
-            // Sending with ticker as key and "TICKER:PRICE" as value
             kafkaTemplate.send(TOPIC_NAME, ticker, messagePayload);
             log.info("Message sent to Kafka topic '{}': Key='{}', Payload='{}'", TOPIC_NAME, ticker, messagePayload);
+            return ResponseEntity.ok().build();
         } catch (Exception e) {
             log.error("Error sending message to Kafka topic '{}' for ticker {}: {}", TOPIC_NAME, ticker, e.getMessage(), e);
-            // Optionally, rethrow or return an error response
+            return ResponseEntity.internalServerError().build();
         }
     }
+
 }
